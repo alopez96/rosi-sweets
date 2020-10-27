d4f1e6c6e0757d26675f66da8b90a5f8e5b44fa9
Arturo Lopez <arturolopez.developer@gmail.com>
2020-10-27 16:43:31 -0700

    animation on nav

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
 
+    //toggle nav
     burger.addEventListener('click', ()=>{
         console.log('clicked')
         nav.classList.toggle('nav-active');
+    })
+
+    const navLinks = document.querySelectorAll('.nav-links li')
+    //animate
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s`
     })
 }
 
