ad9563e45849d38b79be3328d4ce2c284c50c959
Arturo Lopez <arturolopez.developer@gmail.com>
2020-10-27 17:04:04 -0700

    nav complete

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
 
-    //toggle nav
-    burger.addEventListener('click', ()=>{
+    
+    burger.addEventListener('click', () => {
+        //toggle nav
         console.log('clicked')
         nav.classList.toggle('nav-active');
+
+        //animate
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
+            }
     })
 
-    const navLinks = document.querySelectorAll('.nav-links li')
-    //animate
-    navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s`
+    //burger animation
+    burger.classList.toggle('toggle-nav')
     })
+    
 }
 
 navSlide();
